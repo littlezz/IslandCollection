27995e85c3f503a448285546dc6a13177d5a2a9b
zz <zz.at.field@gmail.com>
2015-07-27 10:16:40 +0800

    ThreadSafeSet

--- core/structurers.py
+++ core/structurers.py
@@ -1,4 +1,14 @@
+import threading
+
 __author__ = 'zz'
 
+
+
 class ThreadSafeSet(set):
-    pass
+    def __init__(self, *args, **kwargs):
+        self._add_lock = threading.Lock()
+        super().__init__(*args, **kwargs)
+
+    def add(self, *args, **kwargs):
+        with self._add_lock:
+            return super().add(*args, **kwargs)
