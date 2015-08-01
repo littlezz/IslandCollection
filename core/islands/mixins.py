8a0f3bb07d0604f1741d399471dc696a9026c93b
zz <zz.at.field@gmail.com>
2015-08-01 16:17:09 +0800

    add current_page_url argument to next_page

--- core/islands/mixins.py
+++ core/islands/mixins.py
@@ -10,7 +10,7 @@ class NextPageStaticHtmlMixin:
     _static_count_pattern = re.compile(r'(\d+)')
 
     def get_next_page(self):
-        path = parse.urlparse(self.current_url).path
+        path = parse.urlparse(self.current_page_url).path
         basename = os.path.basename(path)
         # static html basename must be d.htm(l)
         current_page_num, suffix  = basename.split('.')
@@ -24,7 +24,7 @@ class NextPageStaticHtmlMixin:
 
         # In [73]: parse.urljoin('gg/h.html','gg.html')
         # Out[73]: 'gg/gg.html'
-        return parse.urljoin(self.current_url, next_basename), next_page_num
+        return parse.urljoin(self.current_page_url, next_basename), next_page_num
 
     def next_page_valid(self, next_page_url, page_num):
         return requests.head(next_page_url).ok
@@ -41,7 +41,7 @@ class NextPageJsonParameterMixin:
         max_page = self.get_max_page()
 
 
-        base_url, query = parse.splitquery(self.current_url)
+        base_url, query = parse.splitquery(self.current_page_url)
         if not query:
             query='page=0'
 
