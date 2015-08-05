2bf8b7a07a112de4627405fbabd32f8b25f4545f
zz <zz.at.field@gmail.com>
2015-08-06 00:01:05 +0800

    simple widgets test

--- gui/main.py
+++ gui/main.py
@@ -1,4 +1,15 @@
 from tkinter import *
 from tkinter import ttk
+from .widgets import UrlSelectColumnFrame
 __author__ = 'zz'
 
+root = Tk()
+root.title('Island Collections')
+
+main_frame = ttk.Frame(root)
+main_frame.grid(column=0, row=0)
+for i in range(10):
+    u = UrlSelectColumnFrame(main_frame)
+    u.grid(column=0, row=i)
+
+root.mainloop()
---
+++ gui/widgets.py
@@ -0,0 +1,29 @@
+from tkinter import ttk
+import tkinter
+__author__ = 'zz'
+
+
+class CheckButton(ttk.Checkbutton):
+    def __init__(self, master, **kwargs):
+        var = tkinter.IntVar()
+        kwargs.update(variable=var)
+        super().__init__(master, **kwargs)
+
+
+class UrlSelectColumnFrame(ttk.Frame):
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
+        self.check_button = CheckButton(self)
+        self.url_text = ttk.Entry(self, text='url')
+        self.response_num_text = ttk.Entry(self, text='response')
+        self.max_page_text = ttk.Entry(self, text='max page')
+        self.delete_button = ttk.Button(self, text='Delete')
+
+        self.check_button.grid(column=0, row=0)
+        self.check_button.state(['!disabled'])
+        self.url_text.grid(column=1, row=0)
+        self.response_num_text.grid(column=2, row=0)
+        self.max_page_text.grid(column=3, row=0)
+        self.delete_button.grid(column=4, row=0)
+
+
