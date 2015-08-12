eb599da50679edc8aa3990799af651612fd8104e
zz <zz.at.field@gmail.com>
2015-08-12 22:35:35 +0800

    change frame

--- gui/first/__init__.py
+++ gui/first/__init__.py
@@ -1,1 +1,3 @@
 __author__ = 'zz'
+
+from .frames import MainFrame
--- gui/main.py
+++ gui/main.py
@@ -1,21 +1,25 @@
 from tkinter import *
 from tkinter import ttk
 
-from gui.second.frames import MainFrame
+from gui import first, second
 
 __author__ = 'zz'
 
 
-def next_frame(master):
-    master.grid_forget()
-
+def change_frame(self, to):
+    self.grid_forget()
+    to.grid(column=0, row=0)
 
 root = Tk()
 root.title('Island Collections')
 root.minsize(width=666, height=666)
 
-mainframe = MainFrame(root)
-mainframe.grid(column=0, row=0, sticky='NWSE')
+f1 = first.MainFrame(root)
+f2 = second.MainFrame(root)
+f1.grid(column=0, row=0, sticky='NWSE')
+
+f1.foot_frame.button.configure(command=lambda : change_frame(f1, f2))
+f2.foot_frame.button.configure(command=lambda : change_frame(f2, f1))
 
 # ttk.Button(text='next', command=lambda : next_frame(mainframe)).grid(column=1, row=1)
 
