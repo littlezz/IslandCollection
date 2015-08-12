fec1ab4f4168ff47b0fb335642de192d957f09fb
zz <zz.at.field@gmail.com>
2015-08-12 22:55:41 +0800

    FrameStack

--- gui/main.py
+++ gui/main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter
 from tkinter import ttk
 
 from gui import first, second
@@ -6,11 +6,47 @@ from gui import first, second
 __author__ = 'zz'
 
 
+class FrameStack:
+    def __init__(self, frame_list, now=None):
+        self._frames = list(frame_list)
+        self._now = now or self._frames[0]
+
+    def previous_frame(self):
+        """
+        change to previous frame
+        :return:
+        """
+        now = self._now
+        index = self._frames.index(now)
+        to = self._frames[index-1]
+        self.change_frame(now, to)
+
+
+    def next_frame(self):
+        now = self._now
+        index = self._frames.index(now)
+        to = self._frames[index+1]
+        self.change_frame(now, to)
+
+
+    def change_frame(self, now, to):
+        now.grid_forget()
+        to.grid(**self._get_grid_kwargs())
+        self._now = to
+
+    def _get_grid_kwargs(self):
+        return {
+            'column': 0,
+            'row': 0,
+        }
+
+
+
 def change_frame(self, to):
     self.grid_forget()
     to.grid(column=0, row=0)
 
-root = Tk()
+root = tkinter.Tk()
 root.title('Island Collections')
 root.minsize(width=666, height=666)
 
@@ -18,8 +54,10 @@ f1 = first.MainFrame(root)
 f2 = second.MainFrame(root)
 f1.grid(column=0, row=0, sticky='NWSE')
 
-f1.foot_frame.button.configure(command=lambda : change_frame(f1, f2))
-f2.foot_frame.button.configure(command=lambda : change_frame(f2, f1))
+fs = FrameStack(frame_list=[f1, f2], now=f1)
+
+f1.foot_frame.button.configure(command=fs.next_frame)
+f2.foot_frame.button.configure(command=fs.previous_frame)
 
 # ttk.Button(text='next', command=lambda : next_frame(mainframe)).grid(column=1, row=1)
 
