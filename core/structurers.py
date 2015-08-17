0aca9e32772229fb1f3886e7dbc3a23be3137c8f
zz <zz.at.field@gmail.com>
2015-08-17 14:32:36 +0800

    rewrite filter method

--- core/structurers.py
+++ core/structurers.py
@@ -18,24 +18,45 @@ class ThreadSafeSet(set):
 class LookUp:
     def __init__(self, **kwargs):
         lp, rp = kwargs.popitem()
-        self.name, self.operator = self.split_clause(lp)
+        self.name, self.operator = self._split_clause(lp)
         self.value = rp
 
-    def split_clause(self, s):
+    def _split_clause(self, s):
         clauses = s.split('__')
         if len(clauses) == 1:
             return clauses[0], None
         else:
             return clauses[0], clauses[1]
 
+    def find(self, data):
+        """
+        validate the data if satisfy this lookup
+        :return: True or False
+        """
 
 
 class FilterableList(UserList):
     # todo: rewrite filter method
+    # def filter(self, **kwargs):
+    #     ret = self.__class__()
+    #
+    #     for i in self.data:
+    #         if all(getattr(i, name)==value for name, value in kwargs.items()):
+    #             ret.append(i)
+    #
+    #     return ret
     def filter(self, **kwargs):
-        ret = []
-        for i in self.data:
-            if all(getattr(i, name)==value for name, value in kwargs.items()):
-                ret.append(i)
+        """
+        :param kwargs: one key-value pattern
+        :return:filterable list
+        """
+        ret = self.__class__()
+
+        assert len(kwargs) == 1, 'filter method accept one  lookup per time'
+        lookup = LookUp(**kwargs)
+
+        for item in self.data:
+            if lookup.find(item):
+                ret.append(item)
 
         return ret
