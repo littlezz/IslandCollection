bec87d3db044ecbeffdc96742b4bb346a4ca741c
zz <zz.at.field@gmail.com>
2015-08-19 18:38:33 +0800

    koukuko

---
+++ core/islands/koukuko.py
@@ -0,0 +1,37 @@
+from .base import BaseIsland
+from core.islands.mixins import NextPageJsonParameterMixin
+from urllib import parse
+__author__ = 'zz'
+
+
+class KoukokuIsland(NextPageJsonParameterMixin, BaseIsland):
+    _island_name = 'koukoku'
+    _island_netloc = 'kukuku.cc'
+    _static_root = 'http://static.koukuko.com/h/'
+    json_data = True
+
+    def get_tips(self, pd):
+
+        return [thread for thread in pd['data']['threads']]
+
+    def get_div_link(self, tip):
+        thread_id = tip['id']
+        suffix = 't' + '/' + str(thread_id)
+        return parse.urljoin(self.root_url, suffix)
+
+    def get_div_content_text(self, tip):
+        return tip['content']
+
+    def get_div_response_num(self, tip):
+        return tip['replyCount']
+
+    def get_div_image(self, tip):
+        thumb = tip['thumb']
+        return self.complete_image_link(thumb)
+
+    @staticmethod
+    def init_start_url(start_url):
+        if '.json' not in start_url:
+            parts = parse.urlsplit(start_url)
+            path = parts.path + '.json'
+            return parse.urlunsplit((parts.scheme, parts.netloc, path, parts.query, parts.fragment))
